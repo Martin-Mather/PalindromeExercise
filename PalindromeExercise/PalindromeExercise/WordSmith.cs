098fa52e6cc8022c2fe0017197056b2867f2afc0
Martin Mather <hondaccord96@gmail.com>
2021-02-16 09:59:30 -0700

    done

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            //var reverseWord = word.Reverse();
+            //return word.ToLower().SequenceEqual(reverseWord);
+            //return word.SequenceEqual(word.Reverse());
+            //var reversed = word.Reverse().ToString();
+
+            //if(reversed == word)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+            ////reverse word 
+            var reversed = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+
+            //compare the original with the reversed
+            if (reversed.ToLower() == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            ////return whether they are the same or not
+
+        }
+    }
+}
